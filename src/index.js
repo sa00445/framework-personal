4b5cd64dee685e99cd18e2e25c5ad6692a8d9d50
Syed Abdullah Ali <mtuais95@gmail.com>
2020-10-31 13:28:24 +0400

    Header changes- making it responsive

--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+const increment = () => {
+  return {
+    type: "INCREMENT",
+    payload: 2
+  }
+}
+
+const counter = (state = 0, action) => {
+  if(action.type === "INCREMENT") {
+    return state + action.payload
+  }
+}
+
+
+let store = createStore(counter);
+store.subscribe(()=>{console.log("State: " + store.getState())})
+store.dispatch(increment())
+
+// const myAction = () => {
+//   dispatch(
+//     {
+//       type:"OUR_DATA",
+//       payload: {name: "Abdullah"}
+//     }
+//   )
+// }
+
+// const myReducer = ((state=0,action)=>{
+//   if (action.type === "OUR_DATA") {
+//     return {
+//       ...state,
+//       action.payload
+//     }
+//   }
+// })
+
+// createStore(myReducer)
 
 ReactDOM.render(
   <React.StrictMode>
